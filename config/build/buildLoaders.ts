6760b37189b4028b2f7f64460950e93258fd99f7
Vlad <voiteh44@gmail.com>
2024-12-29 20:53:55 +0300

    add file loader,svg loader, types for svg, Button ui kit, ThemeSwitcher

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,20 @@ import { BuildOptions } from "./types/config";
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -30,5 +44,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
 }
